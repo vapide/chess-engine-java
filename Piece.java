cbe390574828fa513ae43a2836769d14dce23ac3
vapide <93092420+vapide@users.noreply.github.com>
2023-03-09 12:54:11 -0700

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,10 +1,10 @@
 public abstract class Piece {
 
-  private String color;
+  private boolean color;
   private int row;
   private int col;
 
-  public Piece(String color, int row, int col) {
+  public Piece(boolean color, int row, int col) {
     this.color = color;
     this.row = row;
     this.col = col;
