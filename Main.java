fc78740c06e90a4e428e09d0d1e2bf2400681bae
vapide <93092420+vapide@users.noreply.github.com>
2023-03-10 09:41:57 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
+        //Pawn pp = new Pawn(true,1,0);
+        //System.out.println(pp.getColor()); works here though???
         Chessboard board = new Chessboard();
-        board.displayBoard();
+        //board.displayBoard();
+        Player player1 = new Player("White", true);
+        Player player2 = new Player("Black", false);
+        Game newGame = new Game(board, player1, player2);
+        newGame.displayBoard();
+        newGame.uciToMove("e2e4");
+        newGame.displayBoard();
         //System.out.println(board.getPieces());
     }
 
