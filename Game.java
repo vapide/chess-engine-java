bdb9478ce67a7ee0cf3906e11d40984d92e0330f
vapide <93092420+vapide@users.noreply.github.com>
2023-03-10 09:39:32 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -8,28 +8,42 @@ public class Game {
         player2 = p2;
     }
 
+    public void displayBoard() {
+        boardClass.displayBoard();
+    }
+
     public int[] uciToInt(String uciString) {
+        // System.out.println(uciString.charAt(1));
         int[] moveVals = new int[4]; // startRow, startCol, endRow, endCol.
+        // Character srow = Character.getNumericValue(Character.valueOf(uciString.charAt(1)));
+        // Character erow = Character.getNumericValue(Character.valueOf(uciString.charAt(3)));
         if(uciString.length() == 5) { // change for promotion (im too lazy lol!)
             moveVals[0] = boardClass.getFileCodes().get(uciString.charAt(0));
-            moveVals[1] = (int)uciString.charAt(1);
+            moveVals[1] = 7 - (int)uciString.charAt(1) + 48;
             moveVals[2] = boardClass.getFileCodes().get(uciString.charAt(2));
-            moveVals[3] = (int)uciString.charAt(3);
+            moveVals[3] = 7 - (int)uciString.charAt(3) + 48;
         } else if(uciString.length() == 4) {
             moveVals[0] = boardClass.getFileCodes().get(uciString.charAt(0));
-            moveVals[1] = (int)uciString.charAt(1);
+            moveVals[1] = 7 - (int)uciString.charAt(1) + 48;
             moveVals[2] = boardClass.getFileCodes().get(uciString.charAt(2));
-            moveVals[3] = (int)uciString.charAt(3);
+            moveVals[3] = 7 - (int)uciString.charAt(3) + 48;
         }
+        // System.out.println(moveVals[1]);
         return moveVals;
     }
 
     public void uciToMove(String uciString) {
      int[] moveVals = uciToInt(uciString);
-     Piece myPieece = board.getPieceFromSquare(moveVals[0],moveVals[1]);
-     if(myPiece.isValidMove(boardClass, myPiece.getColor(), moveVals[0],moveVals[1],moveVals[2],moveVals[3])) {
-      board.movePiece(myPiece, moveVals[0], moveVals[1], moveVals[2], moveVals[3]);   
-     }
+     // System.out.println(moveVals[1]);
+     // System.out.println(moveVals[0]);
+     Piece myPiece = boardClass.getPieceFromSquare(moveVals[1],moveVals[0]);
+     // below 2 statements dont work getter methods return null what the freak.
+     //System.out.println(myPiece.getColor());
+     //System.out.println(myPiece.getRow());
+     // System.out.println(MyPiece);
+     // if(myPiece.isValidMove(boardClass, myPiece.getColor(), moveVals[1],moveVals[0],moveVals[3],moveVals[2])) {
+      boardClass.movePiece(myPiece.getColor(), moveVals[1], moveVals[0], moveVals[3], moveVals[2]);   
+     // }
     }
 
     // make method for ucitomove and use ucitoint or something
