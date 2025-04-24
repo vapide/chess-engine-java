d5b57e2d3c271c28a88ca50b7e49b7819f21c116
will <93092420+vapide@users.noreply.github.com>
2025-04-24 13:49:39 -0600

    Update Knight.java
    fixed the bitwise operators

--- Knight.java
+++ Knight.java
@@ -1,110 +1,80 @@
 public class Knight extends Piece {
     private boolean pieceColor;
     private int pieceRow;
-     private int pieceCol;
-     public Knight(boolean color, int row, int col) {
-      super(color, row, col);
-      pieceColor = color;
-      pieceRow = row;
-      pieceCol = col;
-       //bitboard = new Bitboard( 1L << (row * 8 + col));
-     }
-     
-     public boolean getColor() {
-       return this.pieceColor;
-     }
-    
-     public int getRow() {
-       return this.pieceRow;
-     }
-    
-     public int getCol() {
-       return this.pieceCol;
-     }
-    
-  public void changeRow(int row) {
-    pieceRow = row;
-  }
+    private int pieceCol;
 
-  public void changeCol(int col) {
-    pieceCol = col;
-  }
-
-  public char getSymbol() {
-      if(this.pieceColor) {
-          return 'N';
-      } else {
-          return 'n';
-      }
-  }
-
-  public int getSize(int n) {
-    return 2 ^ ((n ^ 2) && -(n < 2)); // prevents the mask from wrapping probably couldve just done 2 columns filled and just shift it but who cares it is what it is
-  }
-
-  public int getDiff(int n) {
-    return (n - 2) & -(n >= 3); // gets the needed shift to fill 5x5 square around knight which is the mask to prevent wrapping of moves which happens if a knight is on the edge of the board
-  }
-
-  public int repeat(int number) {
-    return (number & 0xFFL) * 0x0101010101010101L; // repeats the 8 digit binary number 8 times to get a full bitboard used for getting the columns to cooperate
-  }
-
-  public int fillCol(int n) {
-    return (255 >> (8 - n)) & 255; // fills 0b00000000 with 1s starting from left to right
-  }
-/*
-  public Bitboard getPsudeoLegalMoves(Chessboard board, boolean color) { // do inverse of same color pieces bitboard then and it with the legal moves
-    return color ? (!board.getWhitePieces() && 0b10100001000100000000000100010000101L >> (Long.numberOfTrailingZeros(row * 8 + col) - 17)) : (!board.getBlackPieces() && 0b10100001000100000000000100010000101L >> (Long.numberOfTrailingZeros(row * 8 + col) - 17));
-  }
-*/
-/*
-public Bitboard getLegalMoves(Board board, int row, int col, boolean color) {
-    // Calculate the number of zeroes before the 1
-    int n = Long.numberOfTrailingZeros(row * 8 + col);
-    // Calculate the mask to shift the bitboard by
-    long mask = 0b0000000000000000000000000000000000000000000000000000000111110010L << (n-17);
-    // Shift the bitboard by the mask to get the possible knight moves
-    long moves = 0b10100001000100000000000100010000101L >> (n - 17) & (~(color ? board.getWhitePieces() : board.getBlackPieces()));
-    // Return the possible knight moves
-    return new Bitboard(moves);
-}
-
-     @Override
-     public boolean isValidMove(Chessboard board, boolean color, int startrow, int startcol, int endrow, int endcol) {
-         return ((color ? board.getWhiteKnights() : board.getBlackKnights()) & (0b1L >> (startrow * 8 + startcol))) & (getLegalMoves(board, startrow, startcol, color) & (0b1L >> (endrow * 8 + endcol)));
-      }
+    public Knight(boolean color, int row, int col) {
+        super(color, row, col);
+        pieceColor = color;
+        pieceRow = row;
+        pieceCol = col;
+        //bitboard = new Bitboard( 1L << (row * 8 + col));
     }
-*/
 
-public Bitboard getLegalMoves(Board board, int row, int col, boolean color) {
-    // Calculate the bit position of the starting square
-    int bitPos = (7 - row) * 8 + col;
+    public boolean getColor() {
+        return this.pieceColor;
+    }
 
-    // Create a mask to represent the possible knight moves around the starting square
-    long mask = 0b10100001000100000000000100010000101L << bitPos;
+    public int getRow() {
+        return this.pieceRow;
+    }
 
-    // Apply the mask to the current board state to filter out illegal moves
-    long potentialMoves = mask & ~(color ? board.getWhitePieces() : board.getBlackPieces());
+    public int getCol() {
+        return this.pieceCol;
+    }
 
-    // Create a new Bitboard object with the filtered potential moves
-    return new Bitboard(potentialMoves);
+    public void changeRow(int row) {
+        pieceRow = row;
+    }
+
+    public void changeCol(int col) {
+        pieceCol = col;
+    }
+
+    public char getSymbol() {
+        if (this.pieceColor) {
+            return 'N';
+        } else {
+            return 'n';
+        }
+    }
+
+    public int getSize(int n) {
+        return 2 * ((n * n) & (n < 2 ? 1 : 0)); 
+    }
+
+    public int getDiff(int n) {
+        return (n - 2) & (n >= 3 ? -1 : 0); 
+    }
+
+    public long repeat(int number) { 
+        return (number & 0xFFL) * 0x0101010101010101L;
+    }
+
+    public int fillCol(int n) {
+        return (255 >> (8 - n)) & 255; // fills 0b00000000 with 1s starting from left to right
+    }
+
+    public Bitboard getLegalMoves(Chessboard board, int row, int col, boolean color) {
+        int bitPos = (7 - row) * 8 + col;
+                long mask = 0b10100001000100000000000100010000101L << bitPos;
+        
+                long potentialMoves = mask & ~(color ? board.getWhitePieces().getBitboard() : board.getBlackPieces().getBitboard());
+
+        return new Bitboard(potentialMoves);
+    }
+
+    @Override
+    public boolean isValidMove(Chessboard board, boolean color, int startrow, int startcol, int endrow, int endcol) {
+        
+        int startBitPos = (7 - startrow) * 8 + startcol;
+        int endBitPos = (7 - endrow) * 8 + endcol;
+
+        long startSquare = 1L << startBitPos;
+        long endSquare = 1L << endBitPos;
+
+        Bitboard pieces = color ? board.getWhitePieces() : board.getBlackPieces();
+        return (pieces.getBitboard() & startSquare) != 0 
+            && (getLegalMoves(board, startrow, startcol, color).getBitboard() & endSquare) != 0;
+    }
 }
-// cry
-@Override
-public boolean isValidMove(Chessboard board, boolean color, int startrow, int startcol, int endrow, int endcol) {
-    // Calculate the bit position of the starting and ending squares
-    int startBitPos = (7 - startrow) * 8 + startcol;
-    int endBitPos = (7 - endrow) * 8 + endcol;
-    // Check if the knight is in the correct starting square and if the ending square is a legal move
-    long startSquare = 1L << startBitPos;
-    long endSquare = 1L << endBitPos;
-    return ((color ? board.getWhiteKnights() : board.getBlackKnights()) & startSquare) != 0 
-        && (getLegalMoves(board, startrow, startcol, color).getBitboard() & endSquare) != 0;
-}
-
-
-
-//0b00000000000000000010100001000100000x0000010001000010100000000000
-
-//0b00000000010100001000100000x0000010001000010100000000000000000000
